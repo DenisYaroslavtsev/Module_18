b285252370cc04c101b3dace938dcf4a82dd43bf
DenisYaroslavtsev <deniska7777@list.ru>
2024-12-06 22:43:53 +0300

    Urls и Views. Функциональное и классовое представление

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
-def func(request):
+def func_info(request):
     return render(request, 'func_templates.html')
 
 class ClassTemplate(TemplateView):
